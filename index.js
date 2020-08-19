375e923148e1756317e17270fff41d55d93d9033
zeelib <zvonimirdamjanovic@gmail.comgit config --global user.email zvonimirdamjanovic@gmail.com>
2020-08-19 15:04:32 +0200

    Added game logic, game start, defined two robots for demo purposes/execution.

--- index.js
+++ index.js
@@ -8,17 +8,71 @@
 class Game {
   constructor(name, playerOne, playerTwo, maxRounds) {
     this._name = name;
-    this._playerOne = playerOne._name;
-    this._playerTwo = playerTwo._name;
+    this._playerOne = playerOne;
+    this._playerTwo = playerTwo;
     this._maxRounds = maxRounds;
     this._currentRound = 1;
+    this._moves = ["Rock", "Paper", "Scissors"];
+  }
+  newGame(){
+    console.log(
+      `We are starting a new game of ${this._name} with players ${this._playerOne._name} and ${this._playerTwo._name} for ${this._maxRounds} rounds.`
+    );
+  
+    for(var i = 1; i <= this._maxRounds; i++) {
+
+      console.log(`Playing round ${this._currentRound} of ${this._maxRounds}`);
+      this._playerOne._move = this.robotMove();
+      this._playerTwo._move = this.robotMove();
+      console.log(this.checkWinner());
+      this._currentRound++;
+
+
+    }
+  }
+  checkWinner(){
+
+    let moveOne = this._playerOne._move;
+    let moveTwo =   this._playerTwo._move;
+
+    if (moveOne === moveTwo) {
+      playerOne._currentScore++;
+      playerTwo._currentScore++;
+      return "Draw";
+    }
+
+    if ((moveOne === "Rock" && moveTwo ==="Scissors") || (moveOne === "Paper" && moveTwo ==="Rock") || (moveOne === "Scissors" && moveTwo ==="Paper")) {
+      playerOne._currentScore++;
+       return `${playerOne._name} wins!`;
+    } else {
+      playerTwo._currentScore++;
+      return `${playerTwo._name} wins!`;
+    }
+
+
+
+
+
+
+  }
+  robotMove(){
+    let randomNumber = Math.random();
+    let choiceIndex  = Math.floor(randomNumber * this._moves.length);
+    console.log((this._moves[choiceIndex]));
+    return (this._moves[choiceIndex]);
+
+
+
   }
 }
+
+//defining class Player
 class Player {
   constructor(name, stats) {
     this._name = name;
     this._stats = stats;
     this._currentScore = 0;
+    this._move = "";
     console.log(`Created player ${name} with an initial score of 0.`);
   }
 }
@@ -26,10 +80,8 @@ const playerOne = new Player("Human", 0);
 const playerTwo = new Player("Robot", 0);
 
 const roshambo = new Game("Roshambo", playerOne, playerTwo, 3);
-console.log(roshambo);
-console.log(
-  `We are starting a new game of ${roshambo._name} with players ${roshambo._playerOne} and ${roshambo._playerTwo} for ${roshambo._maxRounds} rounds.`
-);
+roshambo.newGame();
+
 
 // - Player
 //     - Name
